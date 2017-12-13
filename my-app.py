8a8ca84402d0dd507202fc253d01c2845d452960
SolomonAu-Yeung <34140533+SolomonAu-Yeung@users.noreply.github.com>
2017-12-13 12:46:34 -0500

    Made circles and location variables for each ball

--- my-app.py
+++ my-app.py
@@ -1,7 +1,28 @@
-# Template for Processing sketches.
+p_ball = PVector(500, 300)
+CPT_ball1 = PVector(800, 350)
+CPT_ball2 = PVector(200, 100)
+CPT_ball3 = PVector(120, 500)
+CPT_ball4 = PVector(700, 50)
 
 def setup():
-  size(400, 400)
-  
- def draw():
-  background(255)
+    size(1000, 600)
+    
+def draw():
+    
+    global p_ball
+    global CPT_ball1
+    
+    #player ball
+    ellipse(p_ball.x, p_ball.y, 30, 30)
+
+    #CPT ball 1
+    ellipse(CPT_ball1.x, CPT_ball1.y, 40, 40)
+    
+    #CPT ball 2
+    ellipse(CPT_ball2.x, CPT_ball2.y, 40, 40)
+    
+    #CPT ball 3
+    ellipse(CPT_ball3.x, CPT_ball3.y, 40, 40)
+    
+    #CPT ball 4
+    ellipse(CPT_ball4.x, CPT_ball4.y, 40, 40)
